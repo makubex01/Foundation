69640cadb510687cf6135b7261ea4cf99eb21377
naday <n208v027@gmail.com>
2019-05-26 23:21:47 +0900

    リスト

---
+++ ch5/func_add.py
@@ -0,0 +1,8 @@
+#関数を定義
+def tasu(a,b):
+    return a + b
+
+#関数を使う
+print(tasu(2,5))
+print(tasu(10,30))
+print(tasu(500,240))
---
+++ ch5/func_aquarium.py
@@ -0,0 +1,20 @@
+#水族館の料金を計算する関数を定義
+def calc_fee(age, is_monday):
+    fee = 2000 #一般2000円
+    #年齢に応じて割引
+    if age < 3:
+        fee = 0
+    elif age < 6:
+        fee = 500
+    elif age > 60:
+        fee = 1500
+    #月曜日は2割引
+    if is_monday:
+        fee = int(fee * 0.8)
+    return fee
+
+#関数を利用する
+print(calc_fee(18, False)) #一般(月曜以外)
+print(calc_fee(20, True)) #一般(月曜)
+print(calc_fee(2, False)) #あかちゃん (月曜以外)
+print(calc_fee(70, True)) #シニア(月曜)
